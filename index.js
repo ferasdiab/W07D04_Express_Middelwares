4d4a7bee402f0901cab3109cd88be6fe621a1717
ferasdiab <dfiras96@gmail.com>
2021-05-12 23:04:38 +0300

    solve full

--- index.js
+++ index.js
@@ -4,26 +4,16 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
+const authRouter = express.Router();
+
+
 
 const users = ["John", "Mark"];
 //const users = [];
 
+const products  = ["keyboard", "mouse"]
+
 ///////////////////////////////////////
-
-const logUsers = (req, res, next)=>{
-    console.log(users)
-    next()
-}
-//application-level 
-app.use(logUsers)
-/////////////////////////////////////////////////////
-const logMethod = (req, res, next)=>{
-    console.log(req.method);
-        next()
-}
-
-//app.use("/users",logMethod)
-////////////////////////////////////////
 app.use((req, res, next)=>{
     if (users.length === 0){
         const err = new Error("No users");
@@ -34,14 +24,74 @@ app.use((req, res, next)=>{
     }
 })
 
+///////////
+//practice 6 
+app.use("*",(req, res, next)=>{
+
+        const err = new Error("NOT FOUND");
+        err.status = 404;
+        next(err)
+})
+
+
 
 
 
 //////////////////////////////////
-app.get("/users",logMethod, (req, res, next) => {
+
+
+const logUsers = (req, res, next)=>{
+    console.log(users)
+    next()
+}
+//application-level 
+app.use(logUsers)
+/////////////////////////////////////////////////////
+//midille function 
+
+const logMethod = (req, res, next)=>{
+    console.log(req.method);
+        next()
+}
+
+const middileConsole =(req,res,next)=>{
+    console.log(req.body)
+    next()
+}
+
+//app.use("/users",logMethod)
+////////////////////////////////////////
+//////////////////////////////////
+authRouter.use((req,res,next)=>{
+    console.log("products router")
+    next()
+})
+
+authRouter.get("/users",logMethod, (req, res, next) => {
 
   res.json(users);
 });
+
+/////////////////
+
+
+authRouter.post("/users/create",middileConsole,(req,res,next)=>{
+    const name = req.body.name
+    users.push(name)
+    res.json(users)
+
+})
+
+authRouter.put("/update",(req,res)=>{
+const num = Math.floor(products.length*Math.random())
+products[num] = req.body.name
+res.status(200)
+res.json(products)
+
+})
+
+app.use("/products", authRouter);
+app.use("/users", authRouter);
 
 
 //////////
